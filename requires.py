65c027d206fd9625bf55ff8d6ea3ad3b6d62eb09
Ramprasath <78023316+ramprasath-oss2@users.noreply.github.com>
2022-11-09 22:43:24 +0530

    Add files via upload
    Have fun with this Package

---
+++ requires.py
@@ -0,0 +1,21 @@
+from tkinter import Tk
+
+Number = 0
+
+root = Tk()
+
+WIDTH = 10
+
+FONT = ("Comic Sans MS", 15) # User-defined FONT
+
+RED = "red"
+
+WHITE = "white"
+
+TEAL = "teal"
+
+BLUE = "blue2" 
+
+GREEN = "green2"
+
+PINK = "pink"
---
+++ utils.py
@@ -0,0 +1,44 @@
+from time import strftime
+from tkinter import filedialog, Label
+from pygame import mixer
+from requires import *
+mixer.init()
+
+label1 = Label(root, font=("Old-English", 15), width=10, bg="black", fg="cyan")
+label1.grid(row=0, column=1)
+
+
+def Load():
+    mixer.music.load(filedialog.askopenfile())  # type: ignore 
+    mixer.music.play()
+    #  mixer.music.play(int(input("\nEnter Number of Loops for Play: ")))
+
+
+def Mute():
+    global Number
+    if (Number%2 == 0):
+        mixer.music.pause()
+    if (Number%2 != 0):
+        mixer.music.unpause()
+    Number += 1
+
+
+def Queue():
+    mixer.music.load(filedialog.askopenfile())  # type: ignore 
+    mixer.music.queue(filedialog.askopenfile())  # type: ignore 
+    mixer.music.play()
+    #  mixer.music.play(int(input("\nEnter Number of Loops for Queue: ")))
+
+
+def Replay():
+    mixer.music.play()
+
+
+def Quit():
+    mixer.music.stop()
+    exit()
+
+
+def time():
+    label1.config(text=strftime("%I:%M %p")) 
+    label1.after(1000, time)
