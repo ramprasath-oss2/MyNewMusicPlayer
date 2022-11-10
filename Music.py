02915ee0138dc29d2e8ff7f2853453aa3ede8003
Ramprasath <78023316+ramprasath-oss2@users.noreply.github.com>
2022-11-10 11:45:01 +0530

    Update Music.py

--- Music.py
+++ Music.py
@@ -8,11 +8,11 @@ root.geometry("270x270")
 
 
 def main():
-    Button(text = " Play ", width=WIDTH, font=FONT, fg=WHITE, bg=RED, command=Load).grid(row=1, column=1) 
-    Button(text = " Mute ", width=WIDTH, font=FONT, fg=WHITE, bg=GREEN, command=Mute).grid(row=2, column=1) 
-    Button(text = " Queue", width=WIDTH, font=FONT, fg=WHITE, bg="yellow", command=Queue).grid( row=3, column=1) 
-    Button(text = " Again", width=WIDTH, font=FONT, fg=WHITE, bg=BLUE, command=Replay).grid(row=4, column=1) 
-    Button(text = " Exit ", width=WIDTH, font=FONT, fg=WHITE, bg=PINK, command=Quit).grid(row=5, column=1) 
+    Button(text = " Play ", width=WIDTH, font=FONT, fg=WHITE, bg=RED, command=Load).grid(row=1, column=1, padx=5, pady=4) 
+    Button(text = " Mute ", width=WIDTH, font=FONT, fg=WHITE, bg=GREEN, command=Mute).grid(row=2, column=1, padx=5, pady=4) 
+    Button(text = " Queue", width=WIDTH, font=FONT, fg=WHITE, bg="yellow", command=Queue).grid( row=3, column=1, padx=5, pady=4) 
+    Button(text = " Again", width=WIDTH, font=FONT, fg=WHITE, bg=BLUE, command=Replay).grid(row=4, column=1, padx=5, pady=4) 
+    Button(text = " Exit ", width=WIDTH, font=FONT, fg=WHITE, bg=PINK, command=Quit).grid(row=5, column=1, padx=5, pady=4) 
 
 
 if __name__ == '__main__':
